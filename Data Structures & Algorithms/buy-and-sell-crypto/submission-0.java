3ef648f530b008a12b8eca00e4e609c6eda198db
Riddhima Bhargava <bhargavariddhima@gmail.com>
2026-06-21 20:12:16 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int ans = 0;
+        for(int i = 0;i<prices.length;i++){
+            min = Math.min(min,prices[i]);
+            ans = Math.max(ans,prices[i]-min);
+        }
+        return ans;
+    }
+}
