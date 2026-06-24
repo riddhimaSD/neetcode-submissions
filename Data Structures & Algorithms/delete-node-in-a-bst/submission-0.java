4ccbf10e76cb7b54696b5927740deee2af5e838e
Riddhima Bhargava <bhargavariddhima@gmail.com>
2026-06-24 19:09:05 +0530

    Add: delete-node-in-a-bst - submission-0

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-0.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode func(TreeNode node){
+        while(node.right!=null){
+            node = node.right;
+        }
+        return node;
+    }
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null) return root;
+
+        if(root.val<key){
+            root.right = deleteNode(root.right,key);
+        }
+        else if(root.val>key){
+            root.left = deleteNode(root.left,key);
+        }
+        else{
+            if(root.left==null){
+                return root.right;
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            else{
+                TreeNode pred = func(root.left);
+                root.val = pred.val;
+                root.left = deleteNode(root.left,root.val);
+            }
+        }
+        return root;
+    }
+}
